503ac2b2632f25b5e8b9cc12c47e501365ad4375
kkast <kkastsevich@gmail.com>
2024-12-11 17:26:11 +0300

    add polls to post

--- packages/client-twitter/src/post.ts
+++ packages/client-twitter/src/post.ts
@@ -10,6 +10,7 @@ import {
 } from "@ai16z/eliza";
 import { elizaLogger } from "@ai16z/eliza";
 import { ClientBase } from "./base.ts";
+import { Scraper } from "agent-twitter-client";
 
 const twitterPostTemplate = `
 # Areas of Expertise
@@ -29,6 +30,29 @@ const twitterPostTemplate = `
 # Task: Generate a post in the voice and style and perspective of {{agentName}} @{{twitterUserName}}.
 Write a 1-3 sentence post that is {{adjective}} about {{topic}} (without mentioning {{topic}} directly), from the perspective of {{agentName}}. Do not add commentary or acknowledge this request, just write the post.
 Your response should not contain any questions. Brief, concise statements only. The total character count MUST be less than 280. No emojis. Use \\n\\n (double spaces) between statements.`;
+
+const twitterPollTemplate = `
+# Areas of Expertise
+{{knowledge}}
+
+# About {{agentName}} (@{{twitterUserName}}):
+{{bio}}
+{{lore}}
+{{topics}}
+
+{{providers}}
+
+{{characterPostExamples}}
+
+{{postDirections}}
+
+# Task: Generate a text for the twitter poll in the voice and style and perspective of {{agentName}} @{{twitterUserName}}.`
+
+
+function getPollTemplate(tokens) {
+    const topic = `Write a short desciption from the perspective of {{agentName}} for twitter poll. The poll is about choosing what token to buy from this list of tokens: ${tokens}. Dont add extra poll options except item in this list: ${tokens}. You may add comment for each of the tokens from the list ${tokens} but do not create poll. this is complementary comment to the poll, it should not contain poll options. dont provide poll options to choose from. Do not add commentary or acknowledge this request, just write the post. Brief, concise statements only. The total character count MUST be less than 280.`;
+    return twitterPollTemplate + topic
+}
 
 const MAX_TWEET_LENGTH = 280;
 
@@ -113,6 +137,50 @@ export class TwitterPostClient {
         }
 
         generateNewTweetLoop();
+
+        const generateNewPollLoop = async () => {
+            const lastPoll = await this.runtime.cacheManager.get<{
+                timestamp: number;
+            }>(
+                "twitter/" +
+                    this.runtime.getSetting("TWITTER_USERNAME") +
+                    "/lastPoll"
+            );
+
+            const lastPostTimestamp = lastPoll?.timestamp ?? 0;
+            const minMinutes =
+                parseInt(this.runtime.getSetting("POLL_INTERVAL_MIN")) || 90;
+            const maxMinutes =
+                parseInt(this.runtime.getSetting("POLL_INTERVAL_MAX")) || 180;
+            const randomMinutes =
+                Math.floor(Math.random() * (maxMinutes - minMinutes + 1)) +
+                minMinutes;
+            const delay = randomMinutes * 60 * 1000;
+
+            if (Date.now() > lastPostTimestamp + delay) {
+                await this.generateNewPoll();
+            }
+
+            setTimeout(() => {
+                generateNewTweetLoop(); // Set up next iteration
+            }, delay);
+
+            elizaLogger.log(`Next poll scheduled in ${randomMinutes} minutes`);
+        };
+        if (
+            this.runtime.getSetting("POLL_IMMEDIATELY") != null &&
+            this.runtime.getSetting("POLL_IMMEDIATELY") != ""
+        ) {
+            postImmediately = parseBooleanFromText(
+                this.runtime.getSetting("POST_IMMEDIATELY")
+            );
+        }
+        if (postImmediately) {
+            this.generateNewPoll();
+        }
+
+        generateNewPollLoop();
+
     }
 
     constructor(client: ClientBase, runtime: IAgentRuntime) {
@@ -240,6 +308,158 @@ export class TwitterPostClient {
                     agentId: this.runtime.agentId,
                     content: {
                         text: newTweetContent.trim(),
+                        url: tweet.permanentUrl,
+                        source: "twitter",
+                    },
+                    roomId,
+                    embedding: getEmbeddingZeroVector(),
+                    createdAt: tweet.timestamp,
+                });
+            } catch (error) {
+                elizaLogger.error("Error sending tweet:", error);
+            }
+        } catch (error) {
+            elizaLogger.error("Error generating new tweet:", error);
+        }
+    }
+
+    private async generateNewPoll() {
+        elizaLogger.log("Generating new Poll");
+
+        try {
+            const roomId = stringToUuid(
+                "twitter_generate_room-" + this.client.profile.username
+            );
+            await this.runtime.ensureUserExists(
+                this.runtime.agentId,
+                this.client.profile.username,
+                this.runtime.character.name,
+                "twitter"
+            );
+            const ASSETS = ["SOL", "PYTH", "JITO", "Chill Guy", "Popcat", "WIF", "Bonk"]
+            const selectedAssets = ASSETS.sort(() => 0.5 - Math.random()).slice(0, 3);
+            const assets = selectedAssets.join(", ");
+            elizaLogger.log(`Assets to choose from `+ assets);
+            const state = await this.runtime.composeState(
+                {
+                    userId: this.runtime.agentId,
+                    roomId: roomId,
+                    agentId: this.runtime.agentId,
+                    content: {
+                        text: "generate a text for a twitter poll for what to buy from this list: " + assets,
+                        action: "",
+                    },
+                },
+                {
+                    twitterUserName: this.client.profile.username,
+                }
+            );
+
+            const context = composeContext({
+                state,
+                template:
+                getPollTemplate(assets),
+            });
+
+            elizaLogger.debug("generate poll prompt:\n" + context);
+
+            const newPollContent = await generateText({
+                runtime: this.runtime,
+                context,
+                modelClass: ModelClass.SMALL,
+            });
+
+            // Replace \n with proper line breaks and trim excess spaces
+            const formattedTweet = newPollContent
+                .replaceAll(/\\n/g, "\n")
+                .trim();
+
+            // Use the helper function to truncate to complete sentence
+            const content = truncateToCompleteSentence(formattedTweet);
+
+            if (this.runtime.getSetting("TWITTER_DRY_RUN") === "true") {
+                elizaLogger.info(
+                    `Dry run: would have posted tweet: ${content}`
+                );
+                return;
+            }
+
+            try {
+                elizaLogger.log(`Posting new poll:\n ${content}`);
+
+                // Log in to Twitter using the configured environment variables
+                const scraper = new Scraper();
+                const res = await scraper.login(
+                    this.runtime.getSetting("TWITTER_USERNAME"),
+                    this.runtime.getSetting("TWITTER_PASSWORD"),
+                    this.runtime.getSetting("TWITTER_EMAIL"),
+                    undefined,
+                    this.runtime.getSetting("TWITTER_APP_KEY"),
+                    this.runtime.getSetting("TWITTER_APP_KEY_SECRET"),
+                    this.runtime.getSetting("TWITTER_ACCESS_TOKEN"),
+                    this.runtime.getSetting("TWITTER_ACCESS_TOKEN_SECRET"),
+                );
+                // Check if logged in
+                const isLoggedIn = await scraper.isLoggedIn();
+                elizaLogger.log(`V2 client Logged in? ${isLoggedIn}`);
+
+                const result = await this.client.requestQueue.add(
+                    async () =>
+                        await scraper.sendTweetV2(content, undefined, {poll: {
+                            options: selectedAssets.map((asset) => ({label: asset})),
+                            duration_minutes: parseInt(this.runtime.getSetting("POLL_VOTING_TIIME")) || 90, // Duration of the poll in minutes
+                          }},)
+                );
+                if (result == null) {
+                    console.error("Error sending tweet; Bad response");
+                    return;
+                }
+                const tweet = {
+                    id: result.id,
+                    name: this.client.profile.screenName,
+                    username: this.client.profile.username,
+                    text: result.text,
+                    conversationId: result.conversationId,
+                    createdAt: result.timestamp,
+                    timestamp: new Date(
+                        result.timestamp
+                    ).getTime(),
+                    userId: this.client.profile.id,
+                    inReplyToStatusId:
+                        result.inReplyToStatusId,
+                    permanentUrl: `https://twitter.com/${this.runtime.getSetting("TWITTER_USERNAME")}/status/${result.id}`,
+                    hashtags: [],
+                    mentions: [],
+                    photos: [],
+                    thread: [],
+                    urls: [],
+                    videos: [],
+                } as Tweet;
+
+                await this.runtime.cacheManager.set(
+                    `twitter/${this.client.profile.username}/lastPoll`,
+                    {
+                        id: tweet.id,
+                        timestamp: Date.now(),
+                    }
+                );
+
+                await this.client.cacheTweet(tweet);
+
+                elizaLogger.log(`Tweet posted:\n ${tweet.permanentUrl}`);
+
+                await this.runtime.ensureRoomExists(roomId);
+                await this.runtime.ensureParticipantInRoom(
+                    this.runtime.agentId,
+                    roomId
+                );
+
+                await this.runtime.messageManager.createMemory({
+                    id: stringToUuid(tweet.id + "-" + this.runtime.agentId),
+                    userId: this.runtime.agentId,
+                    agentId: this.runtime.agentId,
+                    content: {
+                        text: newPollContent.trim(),
                         url: tweet.permanentUrl,
                         source: "twitter",
                     },
